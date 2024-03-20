d17412586dd9f484df6ff57d09b2265bfc28e372
Alex <alxjy@ya.ru>
2024-03-20 23:02:04 +0300

    ADD tests

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -13,32 +13,31 @@ from app.users.user_model import Users
 
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
-from app.admin import app as fastapi_app
+from app.main import app as fastapi_app
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 async def prepare_database():
-    assert settings.MODE == 'TEST'
+    assert settings.MODE == "TEST"
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-    
-    def open_mock_json(model:str):
-        with open(f'app/tests/mock_{model}.json', 'r') as file:
+
+    def open_mock_json(model: str):
+        with open(f"tests/mock_{model}.json", "r") as file:
             return json.load(file)
 
-    hotels = open_mock_json('hotels')    
-    rooms = open_mock_json('rooms')    
-    users = open_mock_json('users')    
-    bookings  = open_mock_json('bookings')    
+    hotels = open_mock_json("hotels")
+    rooms = open_mock_json("rooms")
+    users = open_mock_json("users")
+    bookings = open_mock_json("bookings")
 
     for booking in bookings:
         booking["date_from"] = datetime.strptime(booking["date_from"], "%Y-%m-%d")
         booking["date_to"] = datetime.strptime(booking["date_to"], "%Y-%m-%d")
 
-
-    async with async_session_maker() as session: 
+    async with async_session_maker() as session:
         add_hotels = insert(Hotels).values(hotels)
         add_rooms = insert(Rooms).values(rooms)
         add_users = insert(Users).values(users)
@@ -52,7 +51,6 @@ async def prepare_database():
         await session.commit()
 
 
-
 @pytest.fixture(scope="session")
 def event_loop(request):
     """Create an instance of the default event loop for each test case."""
@@ -61,8 +59,22 @@ def event_loop(request):
     loop.close()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 async def ac():
-    async with AsyncClient(app=fastapi_app, base_url='http://test') as ac:
-        yield ac 
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        yield ac
 
+@pytest.fixture(scope="session")
+async def authenticated_ac():
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        await ac.post("/auth/login", json={
+            "email" : "test@test.com",
+            "password" : "test"
+        })
+        assert ac.cookies["booking_access_token"]
+        yield ac
+
+@pytest.fixture(scope="function")
+async def session():
+    async with async_session_maker() as session:
+        yield session
---
+++ app/tests/integration_tests/test_bookings/test_api.py
@@ -0,0 +1,17 @@
+from httpx import AsyncClient
+import pytest 
+
+
+@pytest.mark.parametrize("room_id,date_from,date_to,status_code",[
+    *[(4, "2030-05-01", "2030-05-15", 200)]*8,
+    (4, "2030-05-01", "2030-05-15", 409),
+    (4, "2030-05-01", "2030-05-15", 409),
+])
+async def test_add_and_booking(room_id, date_from, date_to, status_code, authenticated_ac: AsyncClient):
+    response = await authenticated_ac.post('/bookings', params={
+        "room_id" : room_id,
+        "date_from": date_from,
+        "date_to": date_to,
+
+    })
+    assert response.status_code == status_code
