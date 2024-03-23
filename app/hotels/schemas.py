cfa9fa64f17b34fe7707356c9ae789359e2dbf2b
Alex <alxjy@ya.ru>
2024-03-23 23:31:35 +0300

    add new schema for hotels and rooms answer

--- app/hotels/schemas.py
+++ app/hotels/schemas.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from datetime import date
+from typing import List
 
 from fastapi import Query
+from pydantic import BaseModel
 
 
 @dataclass
@@ -11,3 +13,12 @@ class HotelSearchArgs:
     date_to: date
     has_spa: bool = None
     stars: int = Query(None, ge=1, le=5)
+
+class SHotelAndRooms(BaseModel):
+    hotel_id: int
+    name: str
+    location: str
+    services: List[str]
+    image_id: int 
+    total_rooms: int 
+    available_rooms: int 
